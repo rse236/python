b2319885c32701d1297e04cdaca248a46efab584
rse236 <rse236@gmail.com>
2024-01-09 00:12:18 +0900

    Co-authored-by: Daeil Lee <LeeDaeil@users.noreply.github.com>

---
+++ # 2024 - 01 - 08.py
@@ -0,0 +1,5 @@
+# 2024 - 01 - 08
+
+    1. SVG 이미지 넣기 ->> 펌프나 밸브 이미지 호출 [v]
+    2. SVG 이미지 클릭 시 Sub Widget 표시  ->> Controller 생성
+    3. Sub Widget 클릭 시 SVG 이미지 변화  ->> Controller 효과
--- Interface.py
+++ Interface.py
@@ -2,7 +2,6 @@ import sys
 from PyQt5.Qt import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
-from PyQt5.QtGui import QMouseEvent, QResizeEvent
 from PyQt5.QtSvg import *
 from PyQt5.QtWidgets import *
 
@@ -10,7 +9,6 @@ class MainWidget(QWidget):
     def __init__(self) -> None:
         super().__init__()
         self.setGeometry(0, 0, 300, 300)
-        self.count = 0
         self.sub_window = SubWidget(self)
 
         self.scene = QGraphicsScene(self)
@@ -21,15 +19,33 @@ class MainWidget(QWidget):
         vl = QVBoxLayout(self)
         vl.addWidget(self.view)
 
-    def mousePressEvent(self, a0: QMouseEvent) -> None:
-        print(f'Main Widget:{self}')
-        self.sub_window.show()
-        return super().mousePressEvent(a0)
+        self.scene.addItem(ValveSVG(self.sub_window, x = 0, y = 0))
+        self.scene.addItem(ValveSVG(self.sub_window, x = 200, y = 200))
+
+        # def mousePressEvent(self, a0: QMouseEvent) -> None:
+        #  print(f'Main Widget : {self}')
+        #  self.sub_window.show()
+        #  return super().mousePressEvent(a0)
     
     def resizeEvent(self, a0: QResizeEvent) -> None:
         self.scene.setSceneRect(QRectF(0, 0, self.view.size().width() - 2, self.view.size().height() - 2))
         return super().resizeEvent(a0)
     
+class ValveSVG(QGraphicsSvgItem):
+    def __init__(self, sub_window, x=0, y=0):
+        super().__init__()
+        self.setSharedRenderer(QSvgRenderer('./VLVs.svg'))
+        self.setElementId('VLV_AllOpened')
+        self.sub_window = sub_window
+        self.count = 0
+        self.setX(x)
+        self.setY(y)
+    
+    def mousePressEvent(self, event) -> None:
+        self.sub_window.setsvgId(self)
+        self.sub_window.show()
+        return super().mousePressEvent(event)
+
 class SubWidget(QWidget):
     def __init__(self, parent) -> None:
         super().__init__()
@@ -44,16 +60,25 @@ class SubWidget(QWidget):
         vl = QVBoxLayout(self)
         vl.addWidget(self.on_btn)
         vl.addWidget(self.off_btn)
+    
+    def setsvgId(self, widget_id):
+        self.widget_id = widget_id
 
     def mousePressEvent(self, a0: QMouseEvent) -> None:
         print(f'Sub Widget:{self}')
         print(f'Sub Widget:{self.parent}')
         return super().mousePressEvent(a0)
     
-    def call_mainwindow_count_change(self, strategy):
-        self.parent.count = 1 if strategy == 'On' else 0            
-        print(self.parent.count)
+    def show(self) -> None:
+        print(f'{self.widget_id} > {self.widget_id.count}')
+        return super().show()
 
+    def call_mainwindow_count_change(self, strategy):
+        if strategy == 'On':
+            self.widget_id.setElementId('VLV_AllOpened')
+        else:
+            self.widget_id.setElementId('VLV_AllClosed')
+      
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     exe = MainWidget()
